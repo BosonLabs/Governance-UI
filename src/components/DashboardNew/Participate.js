ac1168c21ac4acdb4535bb6f0420280471512451
Prabhakar-git-official <prabhakar4git@gamil.com>
2022-05-16 17:35:35 +0530

    commit on voting status

--- src/components/DashboardNew/Participate.js
+++ src/components/DashboardNew/Participate.js
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import { Col, Container, Row, Form, InputGroup, Button} from 'react-bootstrap';
+import React, {useEffect,useState} from 'react';
+import { Col, Container, Row, Form, InputGroup, Button,Card,Modal} from 'react-bootstrap';
 import Layout from './LayoutT';
 import {
     Link
@@ -9,21 +9,82 @@ import PostCard from './snippets/PostCard';
 import PostCardNew from './snippets/PostCardNew';
 import PostCardElem from './snippets/PostCardElem';
 import { ToastContainer, Toast, Zoom, Bounce, toast} from 'react-toastify';
+import firebase from '../../NFTFolder/firebase';
 import '../toast-style-override.css'
 function Participate() {
     useEffect(() => {
         document.title = "PLANET WATCH | PARTICIPATE"
     }, [])
+    const [commitamount,setcommitamount] = useState("");
+    const dbcallProfile=async()=>{            
+        let r=[];
+        try {     
+        firebase.database().ref("Registeruser").child(localStorage.getItem('walletAddress')).on("value", (data) => {     
+        // firebase.database().ref(`Registeruser/${localStorage.getItem("walletAddress")}`).on("value", (data) => {          
+            if (data) {                        
+       
+              r.push({
+              
+
+                id:data.val().id,
+                WalletAddress:data.val().WalletAddress,
+                TimeStamp:data.val().TimeStamp,
+                Amount:data.val().Amount,
+                Eligibility:data.val().Eligibility,
+                Assettype:data.val().Assettype,
+                Vote:data.val().Vote
+                
+              })                              
+          }
+          else{
+            setcommitamount([""]);  
+          }
+          setcommitamount(r);
+        //   setPlanetAmount(planetAmount);
+               
+        });                  
+      } catch (error) {
+        //console.log('error occured during search', error);    
+      }                
+    }
+    useEffect(()=>{dbcallProfile()},[])
     return (
         <Layout>
             <Container>
                 <Row className='mb-5'>
+                    {commitamount===""||commitamount===undefined||commitamount===null?(<>
+                        <Modal show = {true} className="modal-dashboard" centered >
+            <div className="pt-xl-0 pt-4">   
+                <Link className='text-white mb-20' to="/register"><span className='text-blue'>Go to Register &nbsp;</span>
+                <svg width="22" height="15" viewBox="0 0 22 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M18.6389 8.36952L18.8028 8.2H18.567H0.967033C0.700676 8.2 0.486002 8.10872 0.33782 7.95548C0.189347 7.80195 0.1 7.57826 0.1 7.3C0.1 7.02174 0.189347 6.79805 0.33782 6.64452C0.486002 6.49128 0.700676 6.4 0.967033 6.4H18.567H18.8064L18.6382 6.22972L14.0939 1.63048C14.0937 1.63036 14.0936 1.63023 14.0935 1.63011C13.7445 1.26887 13.7447 0.730627 14.0939 0.369516C14.4414 0.0101614 14.9564 0.0101614 15.3039 0.369516L21.7831 7.06952C21.939 7.23075 21.939 7.46925 21.7831 7.63048L15.3039 14.3305C14.9564 14.6898 14.4414 14.6898 14.0939 14.3305C13.7445 13.9692 13.7445 13.4308 14.0939 13.0695L18.6389 8.36952Z" fill="blue" stroke="currentColor" strokeWidth="0.2"/>
+                                </svg>
+                </Link>
+                </div><br/>
+                <Modal.Header>
+                    <Modal.Title>Please Register before Voting</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    
+                        
+                 
+                </Modal.Body>
+            </Modal>
+                    
+                    
+                    </>):(<>
+                    
                     <Col lg={4} className='mb-4'>
                         <PostCard />
                     </Col>
                     <Col lg={4} className='mb-4'>
                         <PostCardElem />
                     </Col>
+                    
+                    
+                    
+                    </>)}
+                  
                     {/* <Col lg={4} className='mb-4'>
                         <PostCardNew />
                     </Col> */}
